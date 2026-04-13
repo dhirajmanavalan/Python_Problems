8e3e949f073b424b3c266bfe7a626ca4b478f7cb
Dhirajkumar M <kumardhiraj7720@gmail.com>
2026-04-13 10:04:50 +0530

    Logical Programs

---
+++ 12-04-2026/problem1.py
@@ -0,0 +1,23 @@
+'Longest Palindromic Substring'
+s = "babad"
+
+start = 0
+end = 0
+
+def expand(l, r):
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        l -= 1
+        r += 1
+    return r - l - 1
+
+for i in range(len(s)):
+    len1 = expand(i, i)
+    len2 = expand(i, i+1)
+
+    max_len = max(len1, len2)
+
+    if max_len > (end - start):
+        start = i - (max_len - 1) // 2
+        end = i + max_len // 2
+
+print("Longest palindrome:", s[start:end+1])
---
+++ 12-04-2026/problem3.py
@@ -0,0 +1,20 @@
+
+'Combination Sum (Backtracking)'
+candidates = [2,3,6,7]
+target = 7
+
+result = []
+
+def backtrack(start, path, total):
+    if total == target:
+        result.append(path)
+        return
+    if total > target:
+        return
+
+    for i in range(start, len(candidates)):
+        backtrack(i, path + [candidates[i]], total + candidates[i])
+
+backtrack(0, [], 0)
+
+print(result)
